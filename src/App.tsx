b8fb14488051fbea2bd1ecef282d44f98b1222a4
raibove <shwetakale144@gmail.com>
2023-07-14 10:01:38 +0530

    add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { notificationProvider, RefineSnackbarProvider } from "@refinedev/mui";
+import {
+  AuthPage,
+  ErrorComponent,
+  notificationProvider,
+  RefineSnackbarProvider,
+  ThemedLayoutV2,
+} from "@refinedev/mui";
+import { Header } from "./components/header";
+
 
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
+  NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { dataProvider, liveProvider } from "@refinedev/supabase";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import authProvider from "./authProvider";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 import { supabaseClient } from "./utility";
 
+
+
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
+      {/* <GitHubBanner /> */}
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
@@ -36,11 +48,53 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+              <Route
+                  element={
+                    <Authenticated fallback={<CatchAllNavigate to="/login" />}>
+                      <ThemedLayoutV2 Header={() => <Header sticky />}>
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    </Authenticated>
+                  }
+                >
+                {/* <Route index element={<WelcomePage />} /> */}
+                
+                <Route path="*" element={<ErrorComponent />} />
+                </Route>
+                <Route
+                  element={
+                    <Authenticated fallback={<Outlet />}>
+                      <NavigateToResource />
+                    </Authenticated>
+                  }
+                >
+                  <Route
+                    path="/login"
+                    element={
+                      <AuthPage
+                        type="login"
+                        formProps={{
+                          defaultValues: {
+                            email: "info@refine.dev",
+                            password: "refine-supabase",
+                          },
+                        }}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/register"
+                    element={<AuthPage type="register" />}
+                  />
+                  <Route
+                    path="/forgot-password"
+                    element={<AuthPage type="forgotPassword" />}
+                  />
+                </Route>
               </Routes>
               <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
+              {/* <UnsavedChangesNotifier /> */}
+              {/* <DocumentTitleHandler /> */}
             </Refine>
           </RefineSnackbarProvider>
         </ColorModeContextProvider>
--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://hondwvrxgavoswhdpvia.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
-
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvbmR3dnJ4Z2F2b3N3aGRwdmlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg4ODg3NDcsImV4cCI6MjAwNDQ2NDc0N30.nEAiTwBGl1MYvtcuaZJlgS6-nj9x8b2OyRIYdvEzMnc";
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
     schema: "public",
